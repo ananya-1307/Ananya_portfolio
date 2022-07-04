212c23f1989d607f87a81aa9c99abfc738f99146
ananya-1307 <71624430+ananya-1307@users.noreply.github.com>
2022-07-04 17:41:11 +0530

    added new project

--- app.js
+++
@@ -1,6 +0,0 @@
-var menu = document.querySelector('#menu-bars');
-var header = document.querySelector('header');
-menu.onclick = () => {
-    menu.classList.toggle('fa-times');
-    header.classList.toggle('active');
-}
